339ccf37960b3322bc7f7e772e35881ad1c45257
Dima <dima.brashovyan@mail.ru>
2022-07-03 23:13:06 +0300

    Готовая программа для сортировки массива

--- Finish_task/Program.cs
+++ Finish_task/Program.cs
@@ -4,12 +4,15 @@ for(int i = 0; i < array.Length; i++)
 {
     Console.Write($"{array[i]} ");
 }
+Console.WriteLine();
 string[] b = new string [array.Length];
 int j = 0;
 for(int i = 0; i < array.Length; i++)
 {
     if(array[i].Length < 4)
     {
-        
+        b[j] = array[i];
+        Console.Write($"{b[j]} ");
+        j++;
     }
 }
